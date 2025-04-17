b1d1f98c9dc192bd8a0882d114116fb29eb70268
tpavay <pavayt@gmail.com>
2025-04-17 17:53:00 -0400

    Initial Commit

---
+++ Charty/ChartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartyApp.swift
+//  Charty
+//
+//  Created by Tyler Pavay on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ChartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
