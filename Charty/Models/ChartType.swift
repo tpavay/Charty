69534ece60f1540be0a5a23256174a24e50e22cb
tpavay <pavayt@gmail.com>
2025-04-18 12:39:43 -0400

    Add Enums folder within the Models directory and create ChartType enum that contains all the primary charts within the Swift Charts framework.

---
+++ Charty/Models/ChartType.swift
@@ -0,0 +1,20 @@
+//
+//  ChartType.swift
+//  Charty
+//
+//  Created by Tyler Pavay on 4/18/25.
+//
+
+/// Enum comprised of all the primary chart types offered by the Swift Charts framework
+enum ChartType: CaseIterable {
+    case barChart
+    case lineChart
+    case areaChart
+    case pointChart
+    case ruleChart
+    case rectangleChart
+    case symbolChart
+    case pieChart
+    case donutChart
+    case heatmapChart
+}
